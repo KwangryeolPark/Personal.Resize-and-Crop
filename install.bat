415dd38e757761db36f6577b252df4dd8e2345bf
KwangryeolPark <pkr7098@gmail.com>
2024-02-04 17:47:38 +0900

    update

--- install.bat
+++ install.bat
@@ -3,7 +3,10 @@
 npx create-react-app app
 cd app
 npm install react-cropper
+cd ..
 
 copy resources/src/App.js app/src/App.js
 copy resources/src/styles.css app/src/styles.css
+
+cd app
 npm start
